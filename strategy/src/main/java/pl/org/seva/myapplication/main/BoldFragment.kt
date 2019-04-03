bb5bbe665cec0771369207ca323f8ee4d9838536
wiktor <wiktor.nizio@gmail.com>
2019-04-03 22:03:10 +0200

    Renamed the fragments.

--- strategy/src/main/java/pl/org/seva/myapplication/main/BoldFragment.kt
+++ strategy/src/main/java/pl/org/seva/myapplication/main/BoldFragment.kt
@@ -4,23 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fr_second.*
+import kotlinx.android.synthetic.main.fr_bold.*
 import pl.org.seva.myapplication.R
 import pl.org.seva.myapplication.main.extension.emphasis
 import pl.org.seva.myapplication.main.extension.inflate
+import pl.org.seva.myapplication.main.extension.nav
 import pl.org.seva.myapplication.main.kodein.MutableKodeinAware
-import pl.org.seva.myapplication.strategy.Italic
+import pl.org.seva.myapplication.strategy.Bold
 
-class SecondFragment : Fragment(), MutableKodeinAware {
+class BoldFragment : Fragment(), MutableKodeinAware {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
-            inflate(R.layout.fr_second, container)
+            inflate(R.layout.fr_bold, container)
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        kodein.addImport(Italic.MODULE, true)
-        text.text = getString(R.string.second_fragment_prompt)
-                .emphasis(PLACEHOLDER, getString(R.string.second_fragment_replacement))
+        kodein.addImport(Bold.MODULE, true)
+        text.text = getString(R.string.bold_fragment_prompt)
+                .emphasis(PLACEHOLDER, getString(R.string.bold_fragment_replacement))
+        next_fab.setOnClickListener { nav(R.id.action_firstFragment_to_italicFragment) }
     }
 
     companion object {
--- strategy/src/main/java/pl/org/seva/myapplication/main/ItalicFragment.kt
+++ strategy/src/main/java/pl/org/seva/myapplication/main/ItalicFragment.kt
@@ -4,25 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fr_first.*
+import kotlinx.android.synthetic.main.fr_italic.*
 import pl.org.seva.myapplication.R
 import pl.org.seva.myapplication.main.extension.emphasis
 import pl.org.seva.myapplication.main.extension.inflate
-import pl.org.seva.myapplication.main.extension.nav
 import pl.org.seva.myapplication.main.kodein.MutableKodeinAware
-import pl.org.seva.myapplication.strategy.Bold
+import pl.org.seva.myapplication.strategy.Italic
 
-class FirstFragment : Fragment(), MutableKodeinAware {
+class ItalicFragment : Fragment(), MutableKodeinAware {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
-            inflate(R.layout.fr_first, container)
+            inflate(R.layout.fr_italic, container)
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        kodein.addImport(Bold.MODULE, true)
-        text.text = getString(R.string.first_fragment_prompt)
-                .emphasis(PLACEHOLDER, getString(R.string.first_fragment_replacement))
-        next_fab.setOnClickListener { nav(R.id.action_firstFragment_to_secondFragment) }
+        kodein.addImport(Italic.MODULE, true)
+        text.text = getString(R.string.italic_fragment_prompt)
+                .emphasis(PLACEHOLDER, getString(R.string.italic_fragment_replacement))
     }
 
     companion object {
